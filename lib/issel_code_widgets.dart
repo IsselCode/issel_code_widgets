196aead705a8b29ab267d26ad5b94af1bb8d389d
isselcode <isselcode@gmail.com>
2025-12-14 15:13:29 -0600

    exportar dropdown2

--- lib/issel_code_widgets.dart
+++ lib/issel_code_widgets.dart
@@ -4,6 +4,7 @@ export 'package:issel_code_widgets/src/issel_button.dart';
 export 'package:issel_code_widgets/src/issel_carousel.dart';
 export 'package:issel_code_widgets/src/issel_circular_progress_indicator.dart';
 export 'package:issel_code_widgets/src/issel_dropdown.dart';
+export 'package:issel_code_widgets/src/issel_dropdown_2.dart';
 export 'package:issel_code_widgets/src/issel_float_text_field.dart';
 export 'package:issel_code_widgets/src/issel_header_action_tile.dart';
 export 'package:issel_code_widgets/src/issel_info_field.dart';
@@ -16,6 +17,7 @@ export 'package:issel_code_widgets/src/issel_text_form_field.dart';
 export 'package:issel_code_widgets/src/issel_toggle.dart';
 export 'package:issel_code_widgets/src/issel_toggle_field.dart';
 export 'package:issel_code_widgets/src/tab_switcher.dart';
+
 
 // CUSTOMS PAINTS
 export 'package:issel_code_widgets/custom_paints/gradient_border_painter.dart';
