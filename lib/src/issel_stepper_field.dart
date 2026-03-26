cbc1d6b337e832fa75a2f17b5024353f48d78b6a
isselcode <isselcode@gmail.com>
2026-03-26 13:22:35 -0600

    Mejora del stepper field (ahora acepta valores double)

--- lib/src/issel_stepper_field.dart
+++ lib/src/issel_stepper_field.dart
@@ -3,14 +3,14 @@ import 'package:flutter/services.dart';
 
 class IsselStepperField extends StatefulWidget {
 
-  final int initValue;
+  final double initValue;
   final String title;
   final double height;
   final Color? backColor;
   final Color? counterColor;
   final int maxValue;
   final int minValue;
-  final ValueChanged<int> onChanged;
+  final ValueChanged<double> onChanged;
 
   IsselStepperField({
     super.key,
@@ -25,55 +25,77 @@ class IsselStepperField extends StatefulWidget {
   }) : assert(minValue <= maxValue, 'minValue debe ser <= maxValue');
 
   @override
-  State<IsselStepperField> createState() => _IsselStepperFieldState();
+  State<IsselStepperField> createState() => _TestStepperFieldState();
 }
 
-class _IsselStepperFieldState extends State<IsselStepperField> {
+class _TestStepperFieldState extends State<IsselStepperField> {
+
+  FocusNode focus = FocusNode();
 
   @override
   void initState() {
     super.initState();
     delta.text = widget.initValue.toString();
     oldDelta = widget.initValue;
+    focus.addListener(_handleFocusChange);
   }
 
   @override
   void dispose() {
-    super.dispose();
+    focus.removeListener(_handleFocusChange);
+    focus.dispose();
     delta.dispose();
+    super.dispose();
   }
 
-  late int oldDelta;
+  late double oldDelta;
   final TextEditingController delta = TextEditingController();
 
-  void toggleDelta(int value) {
-    if (delta.text.isEmpty || (delta.text.length == 1 && delta.text.contains("-"))){
-     delta.text = oldDelta.toString();
+  void _handleFocusChange() {
+    if (!focus.hasFocus) {
+      onSubmitted(delta.text);
     }
-    int tempValue = int.parse(delta.text);
+  }
+
+  void toggleDelta(double value) {
+    if (delta.text.isEmpty || value == "-" || value == ".") {
+      delta.text = oldDelta.toString();
+    }
+    double tempValue = double.parse(delta.text);
     tempValue += value;
-    int resultado = tempValue.clamp(widget.minValue, widget.maxValue);
+    double resultado = tempValue.clamp(widget.minValue, widget.maxValue).toDouble();
     delta.text = resultado.toString();
     oldDelta = resultado;
-    widget.onChanged.call(int.parse(delta.text));
+    widget.onChanged.call(double.parse(delta.text));
     setState(() {});
   }
 
   void onChanged(String value) {
-    if (value.isEmpty || (value.length == 1 && value.contains("-"))) return;
-    int tempValue = int.parse(delta.text);
-    int resultado = tempValue.clamp(widget.minValue, widget.maxValue);
-    delta.text = resultado.toString();
-    oldDelta = resultado;
-    widget.onChanged.call(int.parse(delta.text));
-    setState(() {});
+    if (value.isEmpty || value == "-" || value == ".") return;
+
+    final parsed = double.tryParse(value);
+    if (parsed == null) return;
+
+    if (parsed > widget.maxValue){
+      double resultado = parsed.clamp(widget.minValue, widget.maxValue).toDouble();
+      delta.text = resultado.toString();
+      oldDelta = resultado;
+      widget.onChanged.call(double.parse(delta.text));
+      setState(() {});
+    }
   }
 
   void onSubmitted(String value) {
     if (delta.text.isEmpty || (delta.text.length == 1 && delta.text.contains("-"))){
       delta.text = oldDelta.toString();
     }
-    widget.onChanged.call(int.parse(delta.text));
+
+    final parsed = double.tryParse(value);
+    if (parsed == null) return;
+    double resultado = parsed.clamp(widget.minValue, widget.maxValue).toDouble();
+    delta.text = resultado.toString();
+    oldDelta = resultado;
+    widget.onChanged.call(double.parse(delta.text));
   }
 
   @override
@@ -87,61 +109,59 @@ class _IsselStepperFieldState extends State<IsselStepperField> {
       width: double.infinity,
       padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
       decoration: BoxDecoration(
-        color: widget.backColor ?? colorScheme.surface,
-        borderRadius: BorderRadius.circular(10)
+          color: widget.backColor ?? colorScheme.surface,
+          borderRadius: BorderRadius.circular(10)
       ),
       child: Row(
         children: [
           // Text
           Expanded(
-            child: Text(
-              widget.title
-            )
+              child: Text(
+                  widget.title
+              )
           ),
 
           // Counter
           Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                color: widget.counterColor ?? theme.scaffoldBackgroundColor,
-                borderRadius: BorderRadius.circular(10)
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    onPressed: () => toggleDelta(-1),
-                    icon: Icon(Icons.remove_outlined)
-                  ),
-                  Expanded(
-                    child: TextField(
-                      controller: delta,
-                      textAlign: TextAlign.center,
-                      style: textTheme.bodyMedium?.copyWith(color: colorScheme.primary, fontWeight: FontWeight.bold),
-                      maxLines: 1,
-                      onSubmitted: onSubmitted,
-                      onChanged: onChanged,
-                      keyboardType: TextInputType.numberWithOptions(signed: true),
-                      inputFormatters: [
-                        FilteringTextInputFormatter.allow(
-                          RegExp(r"^-?\d*$")
+              child: Container(
+                decoration: BoxDecoration(
+                    color: widget.counterColor ?? theme.scaffoldBackgroundColor,
+                    borderRadius: BorderRadius.circular(10)
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                        onPressed: () => toggleDelta(-1),
+                        icon: Icon(Icons.remove_outlined)
+                    ),
+                    Expanded(
+                      child: TextField(
+                        focusNode: focus,
+                        controller: delta,
+                        textAlign: TextAlign.center,
+                        style: textTheme.bodyMedium?.copyWith(color: colorScheme.primary, fontWeight: FontWeight.bold),
+                        maxLines: 1,
+                        onSubmitted: onSubmitted,
+                        onChanged: onChanged,
+                        keyboardType: TextInputType.numberWithOptions(signed: true),
+                        inputFormatters: [
+                          FilteringTextInputFormatter.allow(RegExp(r'^-?\d*([.]\d*)?$')),],
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: oldDelta.toString(),
+                          hintStyle: textTheme.bodyMedium?.copyWith(color: colorScheme.outline),
+                          isCollapsed: true,
                         ),
-                      ],
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: oldDelta.toString(),
-                        hintStyle: textTheme.bodyMedium?.copyWith(color: colorScheme.outline),
-                        isCollapsed: true,
                       ),
                     ),
-                  ),
-                  IconButton(
-                    onPressed: () => toggleDelta(1),
-                    icon: Icon(Icons.add_outlined)
-                  )
-                ],
-              ),
-            )
+                    IconButton(
+                        onPressed: () => toggleDelta(1),
+                        icon: Icon(Icons.add_outlined)
+                    )
+                  ],
+                ),
+              )
           )
         ],
       ),
