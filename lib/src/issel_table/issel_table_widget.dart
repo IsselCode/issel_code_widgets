180cb4a40db28f525d20c2c09f3b84c994c8aff8
isselcode <isselcode@gmail.com>
2026-03-28 20:35:00 -0600

    Correción de detección de fila-3

--- lib/src/issel_table/issel_table_widget.dart
+++ lib/src/issel_table/issel_table_widget.dart
@@ -1,5 +1,3 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
-
 import 'package:flutter/material.dart';
 import 'package:issel_code_widgets/src/issel_table/issel_header_table.dart';
 import 'package:issel_code_widgets/src/issel_table/issel_row_table.dart';
